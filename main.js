82ffb46b33ea3afa712a98f69b06e5ef589ff5a5
rjvelarde0105v@gmail.com <rjvelarde0105v@gmail.com>
2022-05-29 11:51:47 +0800

    nav sticky

--- main.js
+++ main.js
@@ -15,3 +15,23 @@ function toggle() {
 menuBtn.addEventListener("click", () =>{
     menuItems.classList.toggle("show");
 });
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navigation");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
